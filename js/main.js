25d4fd363cc0be0685f4725c88bb924fb0b3f03f
Josue SOlis <josue0509@gmail.com>
2021-06-07 18:59:16 -0600

    Add Filter Type Functions

--- js/main.js
+++ js/main.js
@@ -13,7 +13,6 @@ request.onload = function() {
 }
 
 var nav = document.querySelector('nav');
-//var nav_toggle = document.querySelector('nav-toggle');
 
 function navToggle(){
     nav.classList.toggle('show_nav');
@@ -22,14 +21,34 @@ function navToggle(){
 function displayAll(list){
     data = '<div class="catalogo_list">';
     list.forEach(element => {
-        data += '<div class="card_product">';
-        data += '<img src="https://drive.google.com/uc?id='+element.imagenPortadaUrl+'" alt="">';
+        data += '<div class="card_product" id="'+element.sku+'" data-type="'+element.tipo+'">';
+        data += '<img src="https://drive.google.com/uc?id='+element.imagenPortadaUrl+'" alt="" loading="lazy">';
         data += '<p class="card_name">'+element.nombre+'</p>';
         data += '<p class="card_price">'+Intl.NumberFormat("es-CR", {style: "currency", currency: "CRC"}).format(element.precio)+'</p></div>';
         
     });
     data += '</div>';
-    document.querySelector('main').innerHTML = data;
+    document.getElementById('all_list').innerHTML = data;
 }   
+
+var list = document.getElementsByClassName('card_product');
+var tabs = document.getElementsByClassName('tab');
+
+function filter(e){
+    [].forEach.call(tabs,function(el){
+        el.classList.remove('current');
+    });
+    e.classList.add('current');
+    var tipo = e.name;
+    [].forEach.call(list,function(el){
+        if(el.getAttribute('data-type') == tipo){
+            el.classList.add('show_filter');
+        }else{
+            el.classList.remove('show_filter');
+        }
+    });
+}
+
+
 
 // https://drive.google.com/uc?id=
